f64f9dfc2bebfbcc88b71b02413b8458400654c8
alexislaurent1321-gif <alexis.laurent1321@gmail.com>
2026-04-01 07:41:13 +0000

    Deploying to gh-pages from @ alexislaurent1321-gif/mesh_evaluation@2f40ca69bbc61faacea38679dff39fdd6453a2cd 🚀

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['getboundaryedges_0',['getBoundaryEdges',['../classMesh.html#ad60c2eef2ae7c6e6f9db27d865eefec9',1,'Mesh']]]
+  ['getboundaryedges_0',['getBoundaryEdges',['../classMesh.html#a881a3557d22ac4fe200e8f8c417f550f',1,'Mesh']]],
+  ['getedgevalences_1',['getEdgeValences',['../classMesh.html#a43a5cc7770352cbc4c5d22c6c1096aa7',1,'Mesh']]]
 ];
