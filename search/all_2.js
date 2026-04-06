cab307775b1eb9ae99260f35fa743b40d1278422
alexislaurent1321-gif <alexis.laurent1321@gmail.com>
2026-04-06 13:07:50 +0000

    Deploying to gh-pages from @ alexislaurent1321-gif/mesh_analyzer@220d3d14c0d03971cb9598cc432e1ea7ff27b225 🚀

--- search/all_2.js
+++ search/all_2.js
@@ -5,10 +5,11 @@ var searchData=
   ['callback_5ft_2',['callback_t',['../structtinyobj_1_1callback__t.html',1,'tinyobj']]],
   ['changes_3',['Upcoming changes',['../index.html#autotoc_md11',1,'']]],
   ['class_4',['Mesh class',['../index.html#autotoc_md1',1,'']]],
-  ['coming_20days_5',['in the coming days',['../index.html#autotoc_md12',1,'']]],
-  ['compilation_20and_20execution_6',['Compilation and execution',['../index.html#autotoc_md15',1,'']]],
-  ['countuniqueedges_7',['countUniqueEdges',['../classMesh.html#adfbd56782c0881ab15e84e988ccb2f21',1,'Mesh']]],
-  ['createsupertriangle_8',['createSuperTriangle',['../classDelaunay.html#a57eeffddca46639f7db2a836bcf3b8c3',1,'Delaunay']]],
-  ['cube_9',['Cube',['../index.html#autotoc_md7',1,'']]],
-  ['cube_10',['cube',['../index.html#autotoc_md8',1,'cube'],['../index.html#autotoc_md9',1,'stretched cube']]]
+  ['cleanup_5',['cleanup',['../classDelaunay.html#a5aec4c0de4a8690deca69731635d44c1',1,'Delaunay']]],
+  ['coming_20days_6',['in the coming days',['../index.html#autotoc_md12',1,'']]],
+  ['compilation_20and_20execution_7',['Compilation and execution',['../index.html#autotoc_md15',1,'']]],
+  ['countuniqueedges_8',['countUniqueEdges',['../classMesh.html#adfbd56782c0881ab15e84e988ccb2f21',1,'Mesh']]],
+  ['createsupertriangle_9',['createSuperTriangle',['../classDelaunay.html#a57eeffddca46639f7db2a836bcf3b8c3',1,'Delaunay']]],
+  ['cube_10',['Cube',['../index.html#autotoc_md7',1,'']]],
+  ['cube_11',['cube',['../index.html#autotoc_md8',1,'cube'],['../index.html#autotoc_md9',1,'stretched cube']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['calculateaspectratio_0',['calculateAspectRatio',['../classMesh.html#a26955bacd937606838cdc15cbe40e717',1,'Mesh']]],
   ['calculateaspectratios_1',['calculateAspectRatios',['../classMesh.html#a21f697301a3b654302f4541f9f856ab6',1,'Mesh']]],
-  ['countuniqueedges_2',['countUniqueEdges',['../classMesh.html#adfbd56782c0881ab15e84e988ccb2f21',1,'Mesh']]],
-  ['createsupertriangle_3',['createSuperTriangle',['../classDelaunay.html#a57eeffddca46639f7db2a836bcf3b8c3',1,'Delaunay']]]
+  ['cleanup_2',['cleanup',['../classDelaunay.html#a5aec4c0de4a8690deca69731635d44c1',1,'Delaunay']]],
+  ['countuniqueedges_3',['countUniqueEdges',['../classMesh.html#adfbd56782c0881ab15e84e988ccb2f21',1,'Mesh']]],
+  ['createsupertriangle_4',['createSuperTriangle',['../classDelaunay.html#a57eeffddca46639f7db2a836bcf3b8c3',1,'Delaunay']]]
 ];
