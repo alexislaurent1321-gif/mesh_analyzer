76a31922830988f2620eb65511273de1261c9b54
Alexis Laurent <alexis.laurent1321@gmail.com>
2026-04-15 19:33:25 +0200

    add 2D msh loader

--- include/io/loadMsh.h
+++ include/io/loadMsh.h
@@ -3,6 +3,18 @@
 
 #include "mesh/mesh.h"
 
+
+/**
+ * @brief   Load a triangular mesh from a .msh file in Gmsh format
+ * 
+ * @param mesh 
+ * @param path 
+ * @return true 
+ * @return false 
+ */
+bool loadMsh(Mesh<Triangle>& mesh, const std::string& path);
+
+
 /**
  * @brief   Load a tetrahedral mesh from a .msh file in Gmsh format
  * 
