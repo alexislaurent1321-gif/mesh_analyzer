aa2f43f793fc1f8219c5cdb9c3f6251d7a190bf9
alexislaurent1321-gif <alexis.laurent1321@gmail.com>
2026-04-29 18:46:08 +0200

    Refactor mesh structure: consolidate geometry and I/O headers into Enmesh.h and remove unused files

---
+++ include/Enmesh/Enmesh.h
@@ -0,0 +1,14 @@
+#include "geometry/edge.h"
+#include "geometry/point.h"
+#include "geometry/triangle.h"
+#include "geometry/tetrahedron.h"
+
+#include "io/exportToVTK.h"
+#include "io/loadMsh.h"
+#include "io/loadObj.h"
+
+#include "mesh/mesh.h"
+#include "mesh/mesh_analysis.h"
+#include "mesh/mesh_basic_shapes.h"
+#include "mesh/mesh_smoothing.h"
+#include "mesh/mesh_triangulation.h"
